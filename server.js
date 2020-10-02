a7c8e61a023d949a0e57bfc5087f2fed4173ebff
zackster <ftchong235@yahoo.com>
2020-10-02 15:17:20 +0800

    10.1: Correcting server.js mistakes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const enforce = require("express-sslify");
 
 const app = express();
 
-app.use(enforce.HTTPS({trustProtoHeader: true}));
-app.use(serveStatic( root __dirname + '/dist'));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(serveStatic(__dirname + "/dist"));
 app.use(history());
 
-app.listen(port: process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000);
